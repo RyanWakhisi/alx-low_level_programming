6a6bbe1d0b100db16fb20b814f662c3eb85a1a77
RyanWakhisi <brianwakhisi@gmail.com>
2023-06-18 14:44:20 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = 92;
+
+	while (n >= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
