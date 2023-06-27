bdbea026d708b70931d8dedd54af086542ea3d9d
RyanWakhisi <brianwakhisi@gmail.com>
2023-06-26 19:00:39 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,24 @@
+#incluse "main.h"
+#include <string.h>
+/**
+ * puts2 - prints out the number divisible by two
+ * @str: string input
+ *
+ * Return: returns nothing
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int len;
+
+	len = strlen(str);
+	for (i = 0; i <= len; i++)
+	{
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
