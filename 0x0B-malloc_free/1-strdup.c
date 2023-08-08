db3bdd93d3820da45ddc2e274f01fed41f83fd1e
RyanWakhisi <brianwakhisi@gmail.com>
2023-08-08 12:21:04 +0300

    returns a pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <main.h>
+
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: character
+ * Return: 0
+ */
+
+char *_strdup(char *str)
+{
+	char *ryan;
+	int i, r = 0;
+
+	if (str == NULL)
+		return (NULL);
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+
+	ryan = malloc(sizeof(char) * (i + 1));
+
+	if (ryan == NULL)
+		return (NULL);
+
+	for (r = 0; str[r]; r++)
+		ryan[r] = str[r];
+
+	return (ryan);
+}
