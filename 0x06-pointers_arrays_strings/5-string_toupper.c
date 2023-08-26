7b196b4f11c288974b97018682630efa710c5cb0
RyanWakhisi <brianwakhisi@gmail.com>
2023-08-26 15:52:03 +0300

    sttring touppler

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,26 +1,23 @@
 #include "main.h"
+#include <string.h>
+
 /**
- * string_toupper - change all lowercase to uppercase
- * @n: pointer
- *
- * Return: n
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * @str: string to be converted
+ * Return: str
  */
-char *string_toupper(char *n)
+
+char *string_toupper(char *str)
 {
-	int i;
+	int i = 0;
 
-	i = 0;
-	while (n[i] != '\0')
+	while (str[i] != '\0')
 	{
-		int i;
-
-		i = 0;
-		while (n[i] != '\0')
+		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (n[i] >= 'a' && n[i] <= 'z')
-				n[i] = n[i] - 32;
-			i++;
+			str[i] = str[i] - 'a' + 'A';
 		}
-		return (n);
+		I++;
 	}
-
+	return (str);
+}
