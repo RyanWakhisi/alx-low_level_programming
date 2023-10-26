791e23ab2ec0c939470a3d075b2108672f3c5bc4
RyanWakhisi <brianwakhisi@gmail.com>
2023-10-27 01:31:31 +0300

    a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,15 +1,16 @@
-#ifndef _BIT_MANIP_
-#define _BIT_MANIP_
+main.h
 
-int _strlen(const char *s);
-unsigned int binary_to_uint(const char *b);
-int _putchar(char c);
-void print_binary(unsigned long int n);
-void _divide(unsigned long int n);
-int get_bit(unsigned long int n, unsigned int index);
-int set_bit(unsigned long int *n, unsigned int index);
-int clear_bit(unsigned long int *n, unsigned int index);
-int get_endianness(void);
-unsigned int flip_bits(unsigned long int n, unsigned long int m);
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 #endif
+
